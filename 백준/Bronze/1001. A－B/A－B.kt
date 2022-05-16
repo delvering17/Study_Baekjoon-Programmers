106e963c48f51d32bf805c31f1f364b034bd315a
delver1234 <98009770+delver1234@users.noreply.github.com>
2022-05-16 16:37:03 +0900

    [Bronze V] Title: A-B, Time: 92 ms, Memory: 12168 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+fun main() {
+    val a = BufferedReader(InputStreamReader(System.`in`))
+    val b = StringTokenizer(a.readLine())
+    println(b.nextToken().toInt()-b.nextToken().toInt())
+}
