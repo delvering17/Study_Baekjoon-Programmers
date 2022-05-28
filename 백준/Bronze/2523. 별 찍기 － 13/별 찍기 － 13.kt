814b23ebcd7ecbdc94483b37d7bcd0ec2a5d150c
delver <98009770+delvering17@users.noreply.github.com>
2022-05-28 15:51:05 +0900

    Update 별 찍기 － 13.kt

--- 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.kt
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.kt
@@ -3,3 +3,10 @@ fun main() {
     for(i in 1..a) println("*".repeat(i))
     for(i in a-1 downTo 1) println("*".repeat(i))
 }
+
+// for문 한 개
+
+// fun main() {
+//     var a = readln().toInt()
+//     for(i in 1..a*2) if(i>a) println("*".repeat(2*a-i)) else println("*".repeat(i))
+// }
