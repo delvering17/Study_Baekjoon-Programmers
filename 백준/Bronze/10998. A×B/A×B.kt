1d184d86c1066b7854c4efb3938eb9235c96043a
delver <98009770+delvering17@users.noreply.github.com>
2022-06-01 13:20:12 +0900

    Update A×B.kt

--- 백준/Bronze/10998. A×B/A×B.kt
+++ 백준/Bronze/10998. A×B/A×B.kt
@@ -2,3 +2,9 @@ fun main() {
     val (a,b) =readln().split(" ").map {it.toInt()}
     print(a*b)
 }
+
+// short coding
+
+// it의 index를 가져오면 type은 char이므로 it.toInt()는 아스키 값을 가져온다.
+// '0'를 빼줘서 계산하면 끝
+// fun main()= print(readln().let{ (it[0]-'0')*(it[2]-'0')})
