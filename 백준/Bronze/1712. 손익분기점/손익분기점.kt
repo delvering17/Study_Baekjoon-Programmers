b0a8436ef0cd8a38dae32292b3edeed76501605c
delver <98009770+delvering17@users.noreply.github.com>
2022-06-06 16:32:49 +0900

    [Bronze II] Title: 손익분기점, Time: 136 ms, Memory: 20640 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.kt
+++ 백준/Bronze/1712. 손익분기점/손익분기점.kt
@@ -1,12 +1,4 @@
 fun main() {
     var (a,b,c) = readln().split(" ").map { it.toInt() }
-    var cnt = 0;
-    if(b >= c) print(-1)
-    else {
-        while(a >= 0){
-            cnt++
-            a -= (c-b)
-        }
-        print(cnt)
-    }
+    print(if ((c-b)>0) (a/(c-b))+1 else -1)
 }
