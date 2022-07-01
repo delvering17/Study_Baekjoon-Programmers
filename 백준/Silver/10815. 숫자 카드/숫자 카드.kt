9622de0826d0e227bb83d47a1cc1069a1e098cc0
delver <98009770+delvering17@users.noreply.github.com>
2022-07-01 17:07:02 +0900

    [Silver V] Title: 숫자 카드, Time: 4332 ms, Memory: 237632 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val N = readln().toInt()
+    val arr1 = readln().split(" ").map { it.toInt() }.sorted()
+    val M = readln().toInt()
+    val arr2 = readln().split(" ").map { it.toInt() }
+    arr2.forEach { print("${cal(arr1,it)} ") }
+}
+fun cal(arr1: List<Int>, findNum: Int) : Int {
+    var low = 0
+    var high = arr1.size-1
+    var mid: Int;
+    while(low <= high) {
+        mid = (low + high) / 2
+        if(findNum == arr1[mid]) { return  1 }
+        else if(findNum < arr1[mid]) { high = mid - 1 }
+        else low = mid + 1
+    }
+    return 0
+}
