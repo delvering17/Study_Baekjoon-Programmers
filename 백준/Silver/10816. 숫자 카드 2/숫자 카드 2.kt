ba926496bd0bf145a0f7465a40556b46d9b015a6
delver <98009770+delvering17@users.noreply.github.com>
2022-07-06 11:34:33 +0900

    Update 숫자 카드 2.kt

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
@@ -14,3 +14,47 @@ fun main(){
     println(sb.toString())
 
 }
+
+// 다른 풀이 (이분 탐색)
+// fun main() {
+//     val N = readln().toInt()
+//     val a = readln().split(" ").map { it.toInt() }.toMutableList()
+//     val M = readln().toInt()
+//     var b = readln().split(" ").map { it.toInt() }.toMutableList()
+//     val buffer = StringBuffer()
+//     a.sort()
+//     for (num in b) {
+//         val left = lowerBound(a, num)
+//         val right = upperBound(a, num)
+//         buffer.append(right - left).append(" ")
+//     }
+//     println(buffer.toString())
+// }
+
+// private fun lowerBound(arr: MutableList<Int>, num: Int): Int {
+//     var L = 0
+//     var R = arr.size - 1
+//     while (L <= R) {
+//         val mid = (L + R) / 2
+//         if (arr[mid] < num) {
+//             L = mid + 1
+//         } else {
+//             R = mid - 1
+//         }
+//     }
+//     return L
+// }
+
+// private fun upperBound(arr: MutableList<Int>, num: Int): Int {
+//     var L = 0
+//     var R = arr.size - 1
+//     while (L <= R) {
+//         val mid = (L + R) / 2
+//         if (arr[mid] <= num) {
+//             L = mid + 1
+//         } else {
+//             R = mid - 1
+//         }
+//     }
+//     return L
+// }
