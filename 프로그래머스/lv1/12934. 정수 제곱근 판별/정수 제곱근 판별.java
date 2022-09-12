c96ce5088cc6c283e11026233a0782871740b725
delver <98009770+delvering17@users.noreply.github.com>
2022-09-13 00:39:28 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double sqrt = Math.sqrt(n);
+        if(sqrt % 1 > 0) {
+            answer = -1;
+        } else {
+            answer = (long) Math.pow(sqrt + 1, 2);
+        }
+        return answer;
+    }
+}
