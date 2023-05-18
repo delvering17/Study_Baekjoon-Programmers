387fe76feb4a972c75ca11ec7e244a6e44b9d1a9
delver <98009770+delvering17@users.noreply.github.com>
2023-05-19 00:55:24 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int cnt = 0;
+    public void dfs(int l, int d, int sum, int[] arr) {
+        if (l == 4 || d == arr.length) {
+            if (sum == 0 && l == 4) cnt ++;
+        } else {
+            dfs(l + 1, d + 1, sum + arr[d], arr);
+            dfs(l, d + 1, sum, arr);
+        }
+    }
+
+    public int solution(int[] number) {
+        int answer = 0;
+        dfs(1, 0, 0, number);
+        answer = cnt;
+        return answer;
+    }
+}
