5e586602cad421dbda62090137d21ad57aa9009c
delver <98009770+delvering17@users.noreply.github.com>
2024-07-14 02:03:43 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.24 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+          return Arrays.stream(numbers)
+                .map(it -> it * 2)
+                .toArray();
+    }
+}
