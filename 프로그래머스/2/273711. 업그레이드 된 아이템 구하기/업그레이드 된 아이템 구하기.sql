3dd7fd5c97f11e22e00e39875eb522b8299e7192
delver <98009770+delvering17@users.noreply.github.com>
2024-08-12 18:56:14 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,21 @@
+SELECT
+    INFO.ITEM_ID,
+    INFO.ITEM_NAME,
+    INFO.RARITY
+FROM
+    ITEM_INFO AS INFO
+LEFT JOIN
+    ITEM_TREE AS TREE
+    ON INFO.ITEM_ID = TREE.ITEM_ID
+WHERE
+    PARENT_ITEM_ID IN (
+        SELECT
+            ITEM_ID    
+        FROM
+            ITEM_INFO
+        WHERE
+            RARITY = 'RARE'
+    )
+ORDER BY
+    INFO.ITEM_ID DESC
+    
