dbc65c6e9719634e68f45bcaee8c7c8ed4b160eb
delver <98009770+delvering17@users.noreply.github.com>
2024-10-16 01:10:20 +0900

    [Silver I] Title: 미로 탐색, Time: 236 ms, Memory: 20184 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,17 +2,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Main {
+public class Main {
 
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] board = new int[102][102];
-    static int[][] vis = new int[102][102];
-
-    public static void main(String[ ] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        int[][] board = new int[n][m];
+        int[][] distance = new int[n][m];
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
         for (int i = 0; i < n; i++) {
             String[] split = sc.next().split("");
             for (int j = 0; j < m; j++) {
@@ -20,31 +20,35 @@ class Main {
             }
         }
 
+        distance[0][0] = 1;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(0, 0));
-        vis[0][0] = 1;
-        while(!q.isEmpty()) {
+
+        while (!q.isEmpty()) {
             Pair cur = q.poll();
-            if (cur.x + 1 == n && cur.y + 1 == m ) {
-                System.out.println(vis[cur.x][cur.y]);
-                break;
-            }
+
             for (int i = 0; i < 4; i++) {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
-                if (nx >= n || nx < 0 || ny >= m || ny < 0) continue;
-                if (board[nx][ny] == 0 || vis[nx][ny] > 0) continue;
+
+                if (nx < 0 || nx >= n || ny <0 || ny >= m) continue;
+                if (distance[nx][ny] >= 1 || board[nx][ny] == 0) continue;
+
+                distance[nx][ny] = distance[cur.x][cur.y] + 1;
                 q.add(new Pair(nx, ny));
-                vis[nx][ny] = vis[cur.x][cur.y] + 1;
             }
         }
+
+        System.out.println(distance[n-1][m-1]);
     }
 
-}
-class Pair {
-    int x, y;
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public static class Pair {
+        public int x;
+        public int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
