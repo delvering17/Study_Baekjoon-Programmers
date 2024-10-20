5d7223a1eea80985bb2f819c5c515db56f7ef911
delver <98009770+delvering17@users.noreply.github.com>
2024-10-21 01:15:22 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,16 @@
+SELECT 
+    ID, 
+    EMAIL, 
+    FIRST_NAME, 
+    LAST_NAME
+FROM 
+    DEVELOPERS
+WHERE 
+    ID IN (
+        SELECT DISTINCT D.ID
+        FROM DEVELOPERS D
+        JOIN SKILLCODES S
+        ON D.SKILL_CODE & S.CODE = S.CODE
+        WHERE S.NAME = 'C#' OR S.NAME = 'Python'
+    )
+ORDER BY ID
