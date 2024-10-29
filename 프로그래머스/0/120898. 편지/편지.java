5f7eb862b51b05a08467c65d99b95e3025f47700
delver <98009770+delvering17@users.noreply.github.com>
2024-10-30 00:43:54 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        if (message == null) {
+            return 0;
+        }
+        
+        return message.length() * 2;
+    }
+}
