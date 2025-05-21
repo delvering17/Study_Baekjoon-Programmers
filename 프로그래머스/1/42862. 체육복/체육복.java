605193fe0d894f8dd3de725776ed5e26231ac867
Chanwook Song <98009770+delvering17@users.noreply.github.com>
2025-05-21 15:07:15 +0900

    [level 1] Title: 체육복, Time: 6.37 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        Set<Integer> owns = Arrays.stream(lost)
+            .boxed()
+            .collect(Collectors.toSet());
+        Set<Integer> reserveSet = Arrays.stream(reserve)
+            .boxed()
+            .collect(Collectors.toSet());
+        owns.retainAll(reserveSet);
+        
+        Queue<Integer> q = new LinkedList<>();
+        for (int l : lost) q.add(l);
+        
+        int get = 0;
+        for (int r : reserve) {
+            if (owns.contains(r)) {
+                continue;
+            }
+            
+            while (!q.isEmpty() && (q.peek() < r - 1 || owns.contains(q.peek()))) {
+                q.poll();
+            }
+            
+            if (q.isEmpty()) break;
+            if (q.peek() == r - 1 || q.peek() == r + 1) {
+                q.poll();
+                get++;
+            }
+        }
+        
+        return n - lost.length + owns.size() + get;
+    }
+}
