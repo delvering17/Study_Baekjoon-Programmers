0e48f2488385d8359b2ebc0ed5539a7ad26bcf90
Chanwook Song <98009770+delvering17@users.noreply.github.com>
2025-05-26 13:14:43 +0900

    [level 2] Title: H-Index, Time: 0.90 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        
+        for (int i = citations.length; i > 0; i--) {
+            if (citations[citations.length - i] >= i) {
+                return i;
+            }  
+        }
+        
+        return 0;
+    }
+}
