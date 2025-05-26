2e6cab0b7a77d106803350ff0d24139a156dd64e
Chanwook Song <98009770+delvering17@users.noreply.github.com>
2025-05-26 14:13:13 +0900

    [level 2] Title: 가장 큰 수, Time: 225.22 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((s1, s2) -> (s2 + s1).compareTo(s1 + s2))
+            .collect(Collectors.joining(""));
+
+        if (answer.charAt(0) == '0') return "0";
+
+        return answer;
+    }
+}
