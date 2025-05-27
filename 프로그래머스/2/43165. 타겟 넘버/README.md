8124d1f6d39f76d53c62975c520d54d95a7115cc
Chanwook Song <98009770+delvering17@users.noreply.github.com>
2025-05-27 16:40:20 +0900

    [level 2] Title: 타겟 넘버, Time: 4.41 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 83.4 MB, 시간: 5.21 ms
+메모리: 80.5 MB, 시간: 4.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 15일 14:57:07
+2025년 05월 27일 16:40:17
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,24 +3,23 @@ class Solution {
     int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, 0, target);
+        
+        dfs(0, 0, numbers, target);
+        
         return answer;
     }
     
-    public void dfs(int[] numbers, int result, int depth, int target) {
-        // 결과 확인
-        if (depth == numbers.length) {
-            if (target == result) {
-                answer++;
-            }
+    private void dfs(int index, int sum, int[] numbers, int target) {
+        if (index == numbers.length) {
+            if (target == sum) {
+                answer ++;
+            } 
             return;
         }
-            
-        // 재귀
-        dfs(numbers, result + numbers[depth], depth + 1, target);
-        dfs(numbers, result - numbers[depth], depth + 1, target);
+        
+        dfs(index + 1, sum + numbers[index], numbers, target);
+        dfs(index + 1, sum - numbers[index], numbers, target);
     }
-    
     
     
 }
