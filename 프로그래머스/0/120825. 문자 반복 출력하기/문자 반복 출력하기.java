edbd9242ceb7edc610c10997148ccaacd78a6e28
Chanwook Song <98009770+delvering17@users.noreply.github.com>
2026-06-04 16:23:23 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.17 ms, Memory: 64.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (String s : my_string.split("")) {
+            sb.append(s.repeat(n));
+        }
+        
+        return sb.toString();
+    }
+}
